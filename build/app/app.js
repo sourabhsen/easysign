77b28116f529e6596c7d91a5710fec7600c29d69
unknown <SourabhS@TECH-LP-249.techaspect.com>
2015-12-04 00:30:23 +0530

    commit

---
+++ build/app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+(function (window, angular, undefined) {
+
+  angular.module('easysign', ['ui.bootstrap', 'thumbnail', 'ui.router', 'ngDialog']).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /overview
+    $urlRouterProvider.otherwise('/overview');
+
+    $stateProvider.state('overview', {
+      url: '/overview',
+      views: {
+        'main': {
+          templateUrl: 'app/views/overview/overview.html'
+        },
+        'header': {
+          templateUrl: 'app/views/header.html'
+        }
+      }
+
+    });
+  }]).controller('AppController', [function () {}]);
+})(window, window.angular);
---
+++ src/app/views/overview/overview-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+(function(window, angular, undefined){
+
+  function OverviewController(){}
+
+  OverviewController.$inject = [];
+
+  angular.module('easysign')
+  .controller('OverviewController', OverviewController);
+
+})(window, window.angular);
